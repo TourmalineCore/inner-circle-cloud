cc49f9473b14d97d48fa6749661a742474960d70
Maxim Rychkov <rychkov03@yandex.ru>
2025-12-16 10:26:33 +0500

    chore: rename fuctions

---
+++ functions.tf
@@ -0,0 +1,78 @@
+// This service account is used to enable/disable VM according to schedule
+resource "yandex_iam_service_account" "vm_state_manager_service_account" {
+  name = format("%s-vm-state-manager-service-account", local.resource_name_prefix)
+}
+
+// Grant functions.functionInvoker permission for created service account to invoke cloud functions. (e.g. start/stop VM function) 
+resource "yandex_resourcemanager_folder_iam_member" "vm_state_manager_service_account_function_invoker_iam" {
+  folder_id = var.folder_id
+  role = "functions.functionInvoker"
+  member = "serviceAccount:${yandex_iam_service_account.vm_state_manager_service_account.id}"
+}
+
+// Grant compute.operator permission for created service account to manage VM state
+resource "yandex_resourcemanager_folder_iam_member" "vm_state_manager_service_account_compute_operator_iam" {
+  folder_id = var.folder_id
+  role = "compute.operator"
+  member = "serviceAccount:${yandex_iam_service_account.vm_state_manager_service_account.id}"
+}
+
+resource "yandex_function" "start_vm_function" {
+  name = format("%s-start-vm-function", local.resource_name_prefix)
+  // time stamp for user hash is used to update uploaded functions-js.zip archive content
+  user_hash = "${timestamp()}"
+  runtime = "nodejs18"
+  entrypoint = "index.handler"
+  memory = "128"
+  execution_timeout = "3"
+  service_account_id = yandex_iam_service_account.vm_state_manager_service_account.id
+  folder_id = var.folder_id
+  environment = {
+    FOLDER_ID = var.folder_id
+    INSTANCE_ID = yandex_compute_instance.vm.id
+  }
+  content {
+    zip_filename = "./start-vm-function/function-js.zip"
+  }
+}
+
+resource "yandex_function" "stop_vm_function" {
+  name = format("%s-stop-vm-function", local.resource_name_prefix)
+  // time stamp for user hash is used to update uploaded functions-js.zip archive content
+  user_hash = "${timestamp()}"
+  runtime = "nodejs18"
+  entrypoint = "index.handler"
+  memory = "128"
+  execution_timeout = "3"
+  service_account_id = yandex_iam_service_account.vm_state_manager_service_account.id
+  folder_id = var.folder_id
+  environment = {
+    FOLDER_ID = var.folder_id
+    INSTANCE_ID = yandex_compute_instance.vm.id
+  }
+  content {
+    zip_filename = "./stop-vm-function/function-js.zip"
+  }
+}
+
+resource "yandex_function_trigger" "start_vm_trigger" {
+  name = format("%s-start-vm-trigger", local.resource_name_prefix)
+  timer {
+    cron_expression = "30 1 ? * MON-FRI *"
+  }
+  function {
+    id = yandex_function.start_vm_function.id
+    service_account_id = yandex_iam_service_account.vm_state_manager_service_account.id
+  }
+}
+
+resource "yandex_function_trigger" "stop_vm_trigger" {
+  name = format("%s-stop-vm-trigger", local.resource_name_prefix)
+  timer {
+    cron_expression = "0 16 ? * MON-FRI * "
+  }
+  function {
+    id = yandex_function.stop_vm_function.id
+    service_account_id = yandex_iam_service_account.vm_state_manager_service_account.id
+  }
+}
